005fcf0f0a6a5b263c9c7673efa87bf87454fa01
Loryt55 <151726445+Loryt55@users.noreply.github.com>
2023-11-28 16:43:59 +0100

    Refactor methods with streams

--- src/main/java/Controller/Zoo.java
+++ src/main/java/Controller/Zoo.java
@@ -5,7 +5,7 @@ import model.*;
 import java.util.*;
 
 public class Zoo {
-    private List<Animal> animalsList;
+    private final List<Animal> animalsList;
 
     public Zoo() {
         this.animalsList = new ArrayList<>();
@@ -18,85 +18,47 @@ public class Zoo {
         animalsList.add(newAnimal);
     }
 
-    public Animal findHighestAnimalForSpeciesStream(Class<? extends Animal> animalClass) {
-        return null;
-    }
-
     public Animal findHighestAnimalForSpecies(Class<? extends Animal> animalClass) {
-        Animal currentHighest = null;
-
-        for (Animal animal : animalsList) {
-            if (animalClass.isInstance(animal) && (currentHighest == null || (animal.getHeight() > currentHighest.getHeight()))) {
-                currentHighest = animal;
-            }
-        }
-        return currentHighest;
+        return animalsList.stream()
+                .filter(animalClass::isInstance)
+                .max(Comparator.comparingDouble(Animal::getHeight))
+                .orElse(null);
     }
 
     public Animal findLowesAnimalForSpecies(Class<? extends Animal> animalClass) {
-        Animal currentHighest = null;
-
-        for (Animal animal : animalsList) {
-            if (animalClass.isInstance(animal) && (currentHighest == null || (animal.getHeight() < currentHighest.getHeight()))) {
-                currentHighest = animal;
-            }
-        }
-        return currentHighest;
+        return animalsList.stream()
+                .filter(animalClass::isInstance)
+                .min(Comparator.comparingDouble(Animal::getHeight))
+                .orElse(null);
     }
 
     public Animal findHeavierAnimalForSpecies(Class<? extends Animal> animalClass) {
-        Animal currentHeavier = null;
-
-        for (Animal animal : animalsList) {
-            if (animalClass.isInstance(animal) && (currentHeavier == null || (animal.getWeight() > currentHeavier.getWeight()))) {
-                currentHeavier = animal;
-            }
-        }
-        return currentHeavier;
+        return animalsList.stream()
+                .filter(animalClass::isInstance)
+                .max(Comparator.comparingDouble(Animal::getWeight))
+                .orElse(null);
     }
 
-    public Animal findLightestAnimalForSpecies(Class<? extends Animal> animalClass) {
-        Animal currentLighter = null;
-
-        for (Animal animal : animalsList) {
-            if (animalClass.isInstance(animal) && (currentLighter == null || (animal.getWeight() < currentLighter.getWeight()))) {
-                currentLighter = animal;
-            }
-        }
-        return currentLighter;
+    public <T extends Animal> T findLightestAnimalForSpecies(Class<T> animalClass) {
+        return animalClass.cast(animalsList.stream()
+                .filter(animalClass::isInstance)
+                .min(Comparator.comparingDouble(Animal::getWeight))
+                .orElse(null));
     }
 
 
     public TailedAnimal findLongestAnimalTail() {
-        ArrayList<TailedAnimal> tailedList = new ArrayList<>();
-        for (Animal animal : animalsList) {
-            if (animal instanceof TailedAnimal tailedAnimal) {
-                tailedList.add(tailedAnimal);
-            }
-        }
-        TailedAnimal currentLongestTail = tailedList.getFirst();
-        for (TailedAnimal animalsTails : tailedList) {
-            if (animalsTails.getTailLength() > currentLongestTail.getTailLength()) {
-                currentLongestTail = animalsTails;
-            }
-        }
-        return currentLongestTail;
+        return animalsList.stream()
+                .filter(TailedAnimal.class::isInstance).map(TailedAnimal.class::cast)
+                .max(Comparator.comparingDouble(TailedAnimal::getTailLength))
+                .orElse(null);
     }
 
     public WingedAnimal findLargerAnimalWingspan() {
-        ArrayList<WingedAnimal> wingedList = new ArrayList<>();
-        for (Animal animal : animalsList) {
-            if (animal instanceof WingedAnimal wingedAnimal) {
-                wingedList.add(wingedAnimal);
-            }
-        }
-        WingedAnimal currentLargerWingspan = wingedList.getFirst();
-        for (WingedAnimal wingedAnimal : wingedList) {
-            if (wingedAnimal.getWingspan() > currentLargerWingspan.getWingspan()) {
-                currentLargerWingspan = wingedAnimal;
-            }
-        }
-        return currentLargerWingspan;
+        return animalsList.stream()
+                .filter(WingedAnimal.class::isInstance).map(WingedAnimal.class::cast)
+                .max(Comparator.comparingDouble(WingedAnimal::getWingspan))
+                .orElse(null);
     }
 
 }
--- src/main/java/model/WingedAnimal.java
+++ src/main/java/model/WingedAnimal.java
@@ -3,10 +3,14 @@ package model;
 import java.time.LocalDate;
 
 public abstract class WingedAnimal extends Animal {
-    private final double wingspan;
+    private double wingspan;
 
     protected WingedAnimal(String name, String favoriteFood, int age, LocalDate registrationDate, double weight, double height, double wingspan) {
         super(name, favoriteFood, age, registrationDate, weight, height);
+        this.wingspan = wingspan;
+    }
+
+    public void setWingspan(double wingspan) {
         this.wingspan = wingspan;
     }
 
