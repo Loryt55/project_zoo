1088cce10a36fe541d90ff2a2bf004e8569f5403
Loryt55 <151726445+Loryt55@users.noreply.github.com>
2023-11-28 17:06:47 +0100

    Refactor class

--- src/main/java/model/TailedAnimal.java
+++ src/main/java/model/TailedAnimal.java
@@ -3,10 +3,14 @@ package model;
 import java.time.LocalDate;
 
 public abstract class TailedAnimal extends Animal {
-    private final double tailLength;
+    private double tailLength;
 
     protected TailedAnimal(String name, String favoriteFood, int age, LocalDate registrationDate, double weight, double height, double tailLength) {
         super(name, favoriteFood, age, registrationDate, weight, height);
+        this.tailLength = tailLength;
+    }
+
+    public void setTailLength(double tailLength) {
         this.tailLength = tailLength;
     }
 
